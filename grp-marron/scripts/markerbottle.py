abec8226a8890dcc75793e4b5a985b2ffaab8eef
LucasVANG <lulu5986@hotmail.fr>
2022-01-17 09:53:38 +0100

    rassemblement

--- grp-marron/scripts/markerbottle.py
+++ grp-marron/scripts/markerbottle.py
@@ -8,20 +8,31 @@ from geometry_msgs.msg import Pose
 from visualization_msgs.msg import Marker
 from std_msgs.msg import String
 import math as mth
-import random as rdm
 import tf
+from std_srvs.srv import SetBool
 
 rospy.init_node("new_bottle")
 pub = rospy.Publisher(
     '/bottle',
     Marker, queue_size=10
 )
-tfListener = tf.TransformListener()
 
+def print_Bottle(req):
+    global list_bottle
+    msg=''
+    if not any(t[2]>10 for t in list_bottle):
+        return (False,"Aucune bouteille presente")
+    else:
+        for a in range(len(list_bottle)):
+            if list_bottle[a][2]>10:
+                msg+="[x=" + str(round(list_bottle[a][0],2))+","+"y="+ str(round(list_bottle[a][1],2))+"]"
+    return (True, "Voici les coordonnees des bouteilles detectees: " + msg)
+
+
+tfListener = tf.TransformListener()
+rospy.Service('Liste_Bouteille', SetBool,print_Bottle)
 list_bottle=[]
-x=0
-y=0
-i=1
+i=0
 
 def initialize_marker(i,x,y):
     marker = Marker()
@@ -48,7 +59,7 @@ def initialize_marker(i,x,y):
     return marker
 
 def marker(data):
-    global pub,x,y,i,list_bottle
+    global pub,i,list_bottle
     global tfListener
     poseMap= tfListener.transformPose("map", data)
     x=poseMap.pose.position.x
@@ -57,27 +68,24 @@ def marker(data):
  
 
     if not list_bottle:
-        list_bottle=[[x,y]]
+        list_bottle=[[x,y,1]]
+        i+=1
     else:
         
         if all(( mth.sqrt((x-n[0])**2 + (y-n[1])**2))>0.3 for n in list_bottle):
-            list_bottle.append([x,y])
-            print(list_bottle)
-        
-            pub.publish(bouteille)
+            list_bottle.append([x,y,1])
             i+=1
         else:
             for a in range(0,i,1):
-                print(a,list_bottle[a][0],list_bottle[a][1])
                 if (( mth.sqrt((x-list_bottle[a][0])**2 + (y-list_bottle[a][1])**2))<0.2):
                     list_bottle[a][0]=(list_bottle[a][0]+x)/2
                     list_bottle[a][1]=(list_bottle[a][1]+y)/2
-                    print(a,list_bottle[a][0],list_bottle[a][1])
-                    pub.publish(initialize_marker(a,list_bottle[a][0],list_bottle[a][1]))
+                    list_bottle[a][2]+=1
+                    if (list_bottle[a][2])>10:
+                        pub.publish(initialize_marker(a,list_bottle[a][0],list_bottle[a][1]))
                 for b in range(0,a,1):
                     dist = mth.sqrt((list_bottle[b][0]-list_bottle[a][0])**2 + (list_bottle[b][1]-list_bottle[a][1])**2)
                     if dist<0.1:
-                        print("yes")
                         bottle=initialize_marker(a,list_bottle[a][0],list_bottle[a][1])
                         bottle.action=Marker.DELETE
                         list_bottle.pop(a)
@@ -90,13 +98,9 @@ def marker(data):
 
 
 
-
-
-    
     
 def start():
     rospy.Subscriber('/can',PoseStamped, marker)
     rospy.spin()
 if __name__ == '__main__':
     start()
-
---
+++ grp-marron/scripts/vision.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+import rospy
+import cv2
+import numpy as np
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import math
+
+bridge = CvBridge()
+ 
+
+def interpret_image(data):
+    temp_frame=data
+    global bridge,color,lo,hi,color_info,disArr, timeStamp
+    timeStamp= temp_frame.header.stamp
+    frame = bridge.imgmsg_to_cv2(temp_frame, desired_encoding='passthrough')
+    
+    frame=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    cmd=PoseStamped()
+
+    image=cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+
+        
+        # Affichage des composantes HSV sous la souris sur l'image
+    image=cv2.blur(image, (7, 7))
+    mask=cv2.inRange(image, lo, hi)
+    mask=cv2.erode(mask, None, iterations=5)
+    mask=cv2.dilate(mask, None, iterations=5)
+                
+
+    elements=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        
+    if len(elements) > 0:
+        c=max(elements, key=cv2.contourArea)
+        rec=cv2.boundingRect(c)
+        ((x, y), rayon)=cv2.minEnclosingCircle(c)
+        profondeur=disArr[int(y)][int(x)]
+        coorx=int(rec[0]+(rec[2])/2)
+        coorFin=Coor(coorx,profondeur)
+        if 10<rayon and 4000>profondeur>10:
+            cmd=createPoseStampedPub(int(coorFin[0]),int(coorFin[1]))
+            if cmd.pose.position.x<2 and cmd.pose.position.y<1:
+                pub.publish(cmd)
+                    
+ 
+cv2.destroyAllWindows()
+
+def createPoseStampedPub(x,y):
+    cmd=PoseStamped()
+    cmd.header.stamp=timeStamp
+    cmd.header.frame_id='camera_link'
+    cmd.pose.position.x=x/1000
+    cmd.pose.position.x+=0.15
+    cmd.pose.position.y=-y/1000
+    if cmd.pose.position.y<0:
+        cmd.pose.position.y-=0.1
+    else :
+        cmd.pose.position.y+=0.1
+    cmd.pose.position.z=0
+    cmd.pose.orientation.x=0
+    cmd.pose.orientation.y=0
+    cmd.pose.orientation.z=0
+    cmd.pose.orientation.w=1
+    return cmd
+
+
+def Coor(x,pro):
+    
+    angle=43.55*(x-640)/640
+    angle=angle*math.pi/180 # passage en radians
+    return [math.cos(angle) * pro, math.sin( angle ) * pro-35] 
+
+def distance(data):
+    global disArr
+    disArr=np.array(bridge.imgmsg_to_cv2(data,desired_encoding="passthrough"))
+
+def detection():
+    global lo,hi,pub,color_info
+    rospy.init_node('objet', anonymous=True)
+    pub = rospy.Publisher(
+        'can',   
+        PoseStamped, queue_size=10
+    )
+    rospy.loginfo(rospy.get_caller_id() + 'I heard ')
+
+    lo=np.array([95, 235, 230])
+    hi=np.array([110, 255,255])
+    rate=rospy.Rate(10)
+    color_info=(0, 0, 255)
+    rospy.Subscriber('camera/color/image_raw', Image, interpret_image)
+    rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image , distance)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    detection()
